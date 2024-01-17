71eef5a2d3ec95417324afd5bedb6d6ae0e09bf4
aynsha <aissatou.fall240@gmail.com>
2024-01-17 12:04:58 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function App() {
+  return (
+  <>
+    <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Mon App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Accueil</Nav.Link>
+            <Nav.Link href="#features">A Propos</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br/>
+      <CardGroup>
+      <Card>
+        <Card.Img variant="top" src="img/fleur.jpg" style={{width:'30%'}} />
+        <Card.Body>
+          <Card.Title>Fleur de Lyse</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="img/fleur.jpg" style={{width:'30%'}} />
+        <Card.Body>
+          <Card.Title>Fleur de Lyse</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="img/fleur.jpg" style={{width:'30%'}} />
+        <Card.Body>
+          <Card.Title>Fleur de Lyse</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+    </div>
+    </>
+  );
+}
+
+export default App;
